a9a3214cfa1011c257ce500360ad56c3a1c131e7
SyedFarooqHassan789 <farooqhassan92@hotmail.com>
2018-05-09 09:09:24 +0300

    UI added

--- app/src/main/java/fi/bankaccountapp/MainActivity.java
+++ app/src/main/java/fi/bankaccountapp/MainActivity.java
@@ -2,12 +2,26 @@ package fi.bankaccountapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextAccountNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InitializeUI();
+        InitializeData();
+    }
+
+    private void InitializeUI() {
+        editTextAccountNumber = findViewById(R.id.edit_text_account_number);
+
+    }
+
+    private void InitializeData() {
     }
 }
